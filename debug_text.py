83dfd026ae8755c82c62272cef9a0c4b0b6c9838
Xin Wei <542841993@qq.com>
2023-03-22 17:21:25 +0800

    add pocesser

--- debug_text.py
+++ debug_text.py
@@ -11,40 +11,39 @@ optimizer = torch.optim.Adam(model2.parameters())
 
 text1 = ["测试测试测试测试测试1", "测试测试测试测试测试2"]
 inputs = tokenizer(text1, return_tensors='pt', padding="max_length", truncation=True)
+print(inputs)
+for k in inputs:
+    print(k,inputs[k].dtype)
 inputs["image_pixels"] = torch.rand([2, 1, 128, 128, 128])
 inputs = {k: inputs[k] for k in ['input_ids', "image_pixels"]}
 
 print("======")
 
+
 output_imgs = model2(image_pixels=inputs['image_pixels'], ground_truth=inputs['image_pixels'])
-for i in output_imgs:
-    print(i, output_imgs[i].size())
 output_imgs["loss_image"].backward()
-for n, i in model2.named_parameters():
-    print(n, i.grad is None or (torch.sum(i.grad) == 0.0).item())
 optimizer.step()
-optimizer.zero_grad()
+
+
 
 print("======")
+
+
 output_text = model2(input_ids=inputs['input_ids'], labels=inputs['input_ids'])
-for k in output_text:
-    print(k, output_text[k].size())
 output_text["loss_text"].backward()
-for n, i in model2.named_parameters():
-    print(n, i.grad is None or (torch.sum(i.grad) == 0.0).item())
 optimizer.step()
-optimizer.zero_grad()
+
+
 print("======")
+
+
 output_vl = model2(image_pixels=inputs['image_pixels'], input_ids=inputs['input_ids'],
                    ground_truth=inputs['image_pixels'], labels=inputs['input_ids'])
-for k in output_vl:
-    print(k, output_vl[k].size())
 l = output_vl["loss_image"] + output_vl["loss_text"]
 l.backward()
-for n, i in model2.named_parameters():
-    print(n, i.grad is None or (torch.sum(i.grad) == 0.0).item())
 optimizer.step()
-optimizer.zero_grad()
+
+
 print("======")
 
 # for k in output_imgs:
--- modules/data_pipeline.py
+++ modules/data_pipeline.py
@@ -4,9 +4,14 @@ import math
 from datasets import Dataset
 import os
 import pandas as pd
+import glob
+from patch_embedding import ConvTokenizerPS
+
 class ImageProcesser():
-    def __init__(self):
-        pass
+    def __init__(self, norm_max, norm_min):
+        self.norm_max = norm_max
+        self.norm_min = norm_min
+        self.medical_img_ext = {".nii", ".mhd"}  # TODO:add some ext afterward
 
     def padding(self, img, patch_size, target_size, cut_incomplete_patch):
         """
@@ -34,70 +39,40 @@ class ImageProcesser():
                 nh = (h // patch_size) * patch_size
                 n_start = (h - nh) // 2
 
-                img = img[:, n_start:nh+n_start, :, :]
+                img = img[:, n_start:nh + n_start, :, :]
                 h = nh
 
         if w < target_size:
             if cut_incomplete_patch:
                 nw = (w // patch_size) * patch_size
                 n_start = (w - nw) // 2
-                img = img[:,:,  n_start:nw+n_start, :]
+                img = img[:, :, n_start:nw + n_start, :]
                 w = nw
 
         if d < target_size:
             if cut_incomplete_patch:
                 nd = (d // patch_size) * patch_size
                 n_start = (d - nd) // 2
-                img = img[:,:, :,  n_start:nd+n_start]
+                img = img[:, :, :, n_start:nd + n_start]
                 d = nd
 
         attn_h = math.ceil(h / patch_size)
         attn_w = math.ceil(w / patch_size)
         attn_d = math.ceil(d / patch_size)
         attn_target = target_size // patch_size
-        attn_mask = np.ones([attn_h, attn_w, attn_d])
+        attn_mask = np.ones([attn_h, attn_w, attn_d],dtype=np.int64)
 
         img = np.pad(img, [(0, 0), (0, target_size - h), (0, target_size - w), (0, target_size - d)], mode='constant')
-        attn_mask = np.pad(attn_mask , [(0, attn_target - attn_h), (0, attn_target - attn_w), (0,  attn_target - attn_d)], mode='constant')
+        attn_mask = np.pad(attn_mask, [(0, attn_target - attn_h), (0, attn_target - attn_w), (0, attn_target - attn_d)],
+                           mode='constant')
         return img, attn_mask
 
-
-
-if __name__ == "__main__":
-    #
-    #
-    #
-
-
-    import re
-    from transformers import AutoTokenizer
-
-    control_char_re = re.compile('\s')
-    tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
-
-
-    e = pd.read_excel(r"/dataset/medical-beit/1679037883785病例数据.xlsx")
-    dataset = Dataset.from_pandas(e)
-    dataset = dataset.shuffle(seed=42).train_test_split(test_size=0.3)
-    def input_concat(example):
-        return {"input_text":example['影像所见/病理镜下所见']+example['影像诊断']}
-    def strip_control_characters(s):
-        temp = control_char_re.sub( '', s)
-        return temp
-    def strip_controllers(example):
-        return {"input_text":strip_control_characters(example["input_text"])}
-    def tokenize(example):
-        inputs2 = tokenizer(example["input_text"], truncation=True)
-        ret = {"input_ids":inputs2["input_ids"],"word_ids":inputs2.word_ids().copy()}
-        return ret
-
-    medical_img_ext = {".nii", ".mhd"}
-    def get_file_dict(file_path):
+    def get_file_dict(self, file_path):
         f_dict = {}
 
         for fr, fd, fn in os.walk(file_path):
             for i in fn:
-                if os.path.splitext(i)[1] not in medical_img_ext:
+                if os.path.splitext(i)[1] not in self.medical_img_ext:
                     pass
                 ac = i.split(".")[1]
                 full_path = os.path.join(fr, i)
@@ -108,85 +83,136 @@ if __name__ == "__main__":
                     if f_dict[ac][1] < size:
                         f_dict[ac] = (full_path, size)
         return f_dict
-    def img_path(example,file_path_dict):
+
+    def img_path(self, example, file_path_dict):
         path = None
         if example['检查号'] in file_path_dict:
             path = file_path_dict[example['检查号']][0]
-        ret = {"img_path":path}
+        ret = {"img_path": path}
         return ret
 
-    file_path = "/dataset/medical-beit/HMCT"
-    f_dict = get_file_dict(file_path)
+    def load_img(self, path):
+        img = sitk.ReadImage(path)
+        img = sitk.GetArrayFromImage(img)
+        img = img[None, :, :, :].astype("float32")
+        img = (img - self.norm_min) / (self.norm_max - self.norm_min)
+        ret, attn_mask = self.padding(img, 64, 512, True)
+        return ret, attn_mask
 
-    dataset = dataset.map(input_concat)
-    dataset = dataset.map(strip_controllers)
-    dataset = dataset.map(lambda x:img_path(x,f_dict))
 
-    rc = [i for i in dataset["train"].column_names if i not in ['img_path','input_text']]
-    dataset = dataset.map(remove_columns=rc)
-    dataset = dataset.filter(lambda x: x["img_path"] is not None)
-    dataset = dataset.map(tokenize)
-    print(dataset)
-    print(dataset["train"][:3])
+class TextProcesser():
+    def __init__(self):
+        self.control_char_re = re.compile('\s')
 
-    from transformers import default_data_collator
-    import collections
+    def dataset_load(self, path):
+        f_list = glob.glob(os.path.join(path, "*.xlsx"))
+        e_list = []
+        for f in f_list:
+            e = pd.read_excel(f)
+            e_list.append(e)
+        e = pd.concat(e_list)
+        dataset = Dataset.from_pandas(e)
+        dataset = dataset.shuffle(seed=42).train_test_split(test_size=0.3)
+        return dataset
 
-    ip = ImageProcesser()
-    def list_padding(input_list,item,padding_length):
+    def input_concat(self, example):
+        return {"input_text": example['影像所见/病理镜下所见'] + example['影像诊断']}
+
+    def strip_control_characters(self, s):
+        temp = self.control_char_re.sub('', s)
+        return temp
+
+    def strip_controllers(self, example):
+        return {"input_text": self.strip_control_characters(example["input_text"])}
+
+    def tokenize(self, example, tokenizer):
+        inputs2 = tokenizer(example["input_text"], truncation=True)
+        ret = {"input_ids": inputs2["input_ids"], "word_ids": inputs2.word_ids().copy()}
+        return ret
+
+    def dataset_preprocess(self, dataset, tokenizer, ip, f_dict):
+        dataset = dataset.map(self.input_concat)
+        dataset = dataset.map(self.strip_controllers)
+        dataset = dataset.map(lambda x: ip.img_path(x, f_dict))
+
+        rc = [i for i in dataset["train"].column_names if i not in ['img_path', 'input_text']]
+        dataset = dataset.map(remove_columns=rc)
+        dataset = dataset.filter(lambda x: x["img_path"] is not None)
+        dataset = dataset.map(lambda x: self.tokenize(x, tokenizer))
+        return dataset
+
+    def list_padding(self, input_list, item, padding_length):
         length = len(input_list)
         p_length = padding_length - length
         input_list += [item] * p_length
         return input_list
-    def load_img(path,ip):
-        img = sitk.ReadImage(path)
-        img = sitk.GetArrayFromImage(img)
-        img = img[None,:,:,:]
-        img = (img-np.min(img))/(np.max(img)-np.min(img))#TODO:change this
-        ret, attn_mask = ip.padding(img, 64, 512, True)
-        return ret,attn_mask
-    def mask_text(features,mask_prob_text,tokenizer,padding_length=512):
+
+    def whole_word_masking(self, input_ids, word_ids, mask_prob_text):
+        mapping = collections.defaultdict(list)
+        current_word_index = -1
+        current_word = None
+        for idx, word_id in enumerate(word_ids):
+            if word_id is not None:
+                if word_id != current_word:
+                    current_word = word_id
+                    current_word_index += 1
+                mapping[current_word_index].append(idx)
+
+        # Randomly mask words
+        mask = np.random.binomial(1, mask_prob_text, (len(mapping),))
+        attn_mask = [1] * len(input_ids)
+        labels = input_ids.copy()
+        new_labels = [-100] * len(labels)
+        for word_id in np.where(mask)[0]:
+            word_id = word_id.item()
+            for idx in mapping[word_id]:
+                new_labels[idx] = labels[idx]
+                input_ids[idx] = tokenizer.mask_token_id
+        return input_ids, new_labels, attn_mask
+
+    def collect_fn(self, features, mask_prob_text, tokenizer, ip, padding_length=512):
         for feature in features:
             feature.pop("input_text")
             word_ids = feature.pop("word_ids")
             img_path = feature.pop("img_path")
-            img,img_attn = load_img(img_path,ip)
-            # Create a map between words and corresponding token indices
-            mapping = collections.defaultdict(list)
-            current_word_index = -1
-            current_word = None
-            for idx, word_id in enumerate(word_ids):
-                if word_id is not None:
-                    if word_id != current_word:
-                        current_word = word_id
-                        current_word_index += 1
-                    mapping[current_word_index].append(idx)
-
-            # Randomly mask words
-            mask = np.random.binomial(1, mask_prob_text, (len(mapping),))
-            input_ids = feature["input_ids"]
-
-            attn_mask = [1] * len(input_ids)
-
-            list_padding(input_ids,0,padding_length)
-            list_padding(word_ids,None,padding_length)
-            list_padding(attn_mask,0,padding_length)
-            labels = input_ids.copy()
-            new_labels = [-100] * len(labels)
-
-            for word_id in np.where(mask)[0]:
-                word_id = word_id.item()
-                for idx in mapping[word_id]:
-                    new_labels[idx] = labels[idx]
-                    input_ids[idx] = tokenizer.mask_token_id
+            input_ids = feature.pop("input_ids")
+            img, img_attn = ip.load_img(img_path)
+            input_ids, new_labels, attn_mask = self.whole_word_masking(input_ids, word_ids, mask_prob_text)
+            input_ids = self.list_padding(input_ids, tokenizer.pad_token_id, padding_length)
+            new_labels = self.list_padding(new_labels, -100, padding_length)
+            attn_mask = self.list_padding(attn_mask, 0, padding_length)
             feature["labels"] = new_labels
             feature["attention_mask"] = attn_mask
+            feature["input_ids"] = input_ids
             feature["img"] = img.astype("float16")
             feature["img_attn"] = img_attn
+            feature["token_type_ids"] = [0]*padding_length+[1]*512 #TODO: it is fixed in
         return default_data_collator(features)
 
+
+if __name__ == "__main__":
+    #
+    #
+    #
+
+    import re
+    from transformers import AutoTokenizer, default_data_collator
+    import collections
     from torch.utils.data import DataLoader
-    train_dataloader = DataLoader(dataset["train"], shuffle=True, batch_size=2, collate_fn=lambda x:mask_text(x,0.5,tokenizer))
+    import torch
+    device = torch.device("cuda:0")
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-chinese")
+    img_tokenizer = ConvTokenizerPS().half().to(device)
+    print(tokenizer.pad_token_id)
+    tp = TextProcesser()
+    ip = ImageProcesser(norm_max=1000, norm_min=-1000)
+    f_dict = ip.get_file_dict("/dataset/medical-beit/HMCT")
+    dataset = tp.dataset_load(r"/dataset/medical-beit")
+    dataset = tp.dataset_preprocess(dataset, tokenizer, ip, f_dict)
+
+    train_dataloader = DataLoader(dataset["train"], shuffle=True, batch_size=2,
+                                  collate_fn=lambda x: tp.collect_fn(x, 0.5, tokenizer, ip))
     for i in train_dataloader:
-        for k in i:
-            print(k,i[k].size())
+
+        input_img = img_tokenizer.downsample(i['img'].to(device))
+        print(input_img.size())
